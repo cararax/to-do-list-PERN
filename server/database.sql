eb281ba20aff803d1305e54f7004c81ee359f622
pedro Carara <cararax@gmail.com>
2020-11-23 14:49:02 -0300

    api finalizada

---
+++ server/database.sql
@@ -0,0 +1,5 @@
+create database perntodo;
+create table todo(
+    todo_id serial primary key,
+    description varchar(255)
+);
--- server/index.js
+++ server/index.js
@@ -1,6 +1,85 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+const pool = require('./db')
 
-app.listen (5000, () => {
+//Middleware
+app.use(cors());
+app.use(express.json()); //req.body
+
+//Routes
+
+//Create a todo
+app.post('/todos', async (req, res) => {
+    try {
+
+        const { description } = req.body;
+        const newTodo = await pool.query("INSERT INTO todo(description) VALUES ($1) RETURNING *", [description]);
+
+        res.json(newTodo.rows[0]);
+
+    } catch (error) {
+        console.error(error.message);
+    }
+})
+
+//Get all todos 
+app.get("/todos", async (req, res) => {
+    try {
+
+        const allTodos = await pool.query("SELECT * FROM todo")
+
+        res.json(allTodos.rows)
+
+    } catch (error) {
+        console.error(error.message);
+    }
+})
+
+//Get a todo
+app.get('/todos/:id', async (req, res) => {
+    try {
+
+        const { id } = req.params;
+        const todo = await pool.query("SELECT * FROM todo WHERE todo_id=$1", [id]);
+
+        res.json(todo.rows[0])
+
+
+    } catch (error) {
+        console.log(error.message)
+    }
+})
+
+//Update a todo
+app.put('/todos/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { description } = req.body;
+        const updateTodo = await pool.query("UPDATE todo SET description = $1 WHERE todo_id = $2", [description, id])
+
+        res.json("Todo was updated successfully.")
+
+    } catch (error) {
+        console.error(error.message)
+    }
+})
+
+//Delete a todo 
+app.delete('/todos/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deletetodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [id])
+
+        res.json("Todo was deleted successfully.")
+
+
+    } catch (error) {
+        console.log(error.message)
+    }
+})
+
+
+app.listen(5000, () => {
     console.log('Server has started listening on port 5000')
 })
