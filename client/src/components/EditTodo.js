59dcf224bb57ed2e4c8d81cba6bf7ac0145c7824
pedro Carara <cararax@gmail.com>
2020-11-24 10:42:37 -0300

    tentando deploy

--- client/src/components/EditTodo.js
+++ client/src/components/EditTodo.js
@@ -11,12 +11,13 @@ const EditTodo = ({ todo }) => {
         e.preventDefault()
         try {
             const body = { description }
-            const response = await fetch(`http://localhost:5000/todos/${todo.todo_id}`,
+            /*const response = await fetch(`http://localhost:5000/todos/${todo.todo_id}`,
                 {
-                    method: 'PUT',
-                    headers: { 'Content-Type': 'application/json' },
+                    method: "PUT",
+                    headers: { "Content-Type": "application/json" },
                     body: JSON.stringify(body)
-                })
+                }
+            );*/
 
             window.location = '/'
         } catch (error) {
