562d8b0702a467a3894347fcf1d1f4f8217e9094
pedro Carara <cararax@gmail.com>
2020-11-24 10:17:49 -0300

    edit todo finalizado

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useEffect, useState } from 'react';
+import EditTodo from './EditTodo'
 
 const ListTodos = () => {
 
@@ -52,7 +53,7 @@ const ListTodos = () => {
                     {todos.map(todo => (
                         <tr key={todo.id}>
                             <td>{todo.description}</td>
-                            <td>Edit</td>
+                            <td> <EditTodo todo={todo}/> </td>
                             <td><button className='btn btn-danger' onClick={() => deleteTodo(todo.todo_id)}>Delete</button></td>
                         </tr>
                     ))}
